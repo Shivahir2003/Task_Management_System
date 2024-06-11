93c21e3c6ce9bea59023b73d9c9669c23c7ba30c
Shivahir2003 <shivahir0114@gmail.com>
2024-06-11 19:51:38 +0530

    style: minor changes in templates designs

--- task_management_system/task_management_system/urls.py
+++ task_management_system/task_management_system/urls.py
@@ -19,9 +19,13 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from taskmanager.views import DashboardView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls',))
+    path('accounts/',include('accounts.urls',)),
+    path('task/',include('taskmanager.urls',)),
+    path('',DashboardView.as_view())
 ]
 
 if settings.DEBUG:
