cfed7552364a17255a0962f6ed3574580a2f7f82
Shivahir2003 <shivahir0114@gmail.com>
2024-06-11 19:48:25 +0530

    docs: added comments

--- task_management_system/accounts/models.py
+++ task_management_system/accounts/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    """
+        User Profile model
+        
+        Create user's extra details
+    """
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     mobile_number=models.CharField(max_length=10)
     user_image=models.ImageField(upload_to='accounts/',null=True,default=None)
