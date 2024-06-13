1382771b366b7aed602fa1d6ea4483af29348567
Shivahir2003 <shivahir0114@gmail.com>
2024-06-13 20:27:29 +0530

    fix: implemented frontend validation

---
+++ task_management_system/static/js/change_pass_validate.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    // validation for password
+  $.validator.addMethod(
+    "passwordformat",
+    function (value, element) {
+      return (
+        this.optional(element) ||
+        /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/.test(value)
+      );
+    },
+    "Password Is Not Valid"
+  );
+    $("#reset_password").validate({
+        rules: {
+            current_password: {
+                passwordformat: true,
+                required: true,
+            },
+            new_password: {
+                passwordformat: true,
+                required: true,
+            },
+            confirm_password: {
+                required: true,
+            },
+        },
+        messages: {
+            current_password: {
+                required: "current_password is Required",
+            },
+            new_password: {
+                required: "new_password is Required",
+            },
+            confirm_password: {
+                required: "confirm_password is Required",
+            },
+
+        },
+        errorPlacement: function (error, element) {
+            error.appendTo(element.next());
+          },
+    })
+})
